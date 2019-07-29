f915ddacac58f0a5017f7430600c2da49826cc22
Student <kaczanowskamonika@gmail.com>
2019-07-29 14:40:51 +0100

    testing today's date

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,21 +1,18 @@
-require 'date'
 class BankAccount
 
-  attr_reader :statement, :date_of_deposit, :date_of_withdrawal
+  attr_reader :statement
 
   def initialize
     @statement= 0
   end
 
   def add_deposit(amount)
-    @date_of_deposit=Date.today.to_s
 
     @statement += amount
 
   end
 
   def deduct_from_statement(amount)
-    @date_of_withdrawal=Date.today.to_s
 
     @statement -= amount
   end
